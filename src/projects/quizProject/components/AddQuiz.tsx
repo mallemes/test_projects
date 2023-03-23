3e7263a1bcafede74ae043c09c8ea2947bf8efaa
erjan <dosaev.rysbek5@gmail.com>
2023-03-23 19:09:50 +0600

    quiz app add quiz logic

--- src/projects/quizProject/components/AddQuiz.tsx
+++ src/projects/quizProject/components/AddQuiz.tsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from "../style/index.module.css";
+import {QuizDates} from "../data";
+
+type quizType = {
+    questionText: string;
+    answers: Array<{ answer: string; isCorrect: boolean; }>;
+}[]
+type questionType = {
+    questionText: string;
+    answers: Array<{ answer: string; isCorrect: boolean; }>;
+}
+const a12 = {questionText: "",
+    answers: [{answer: "", isCorrect: false}, {answer: "", isCorrect: false}, {
+        answer: "",
+        isCorrect: false
+    }, {answer: "", isCorrect: false}]
+}
 
 const AddQuiz = () => {
+
+    const [isChecked, setIsChecked] = useState(1)
+    const [newQuizValues, setNewQuizValues] = useState<questionType>(a12)
+    const [newQuiz, setNewQuiz] = useState<quizType|null>(null)
+    const inputs = [1, 2, 3, 4]
+    const changeArr = (e: React.ChangeEvent<HTMLInputElement>, i: number) => {
+        const ch = {answer: e.target.value, isCorrect: isChecked - 1 === i}
+        const a2 = newQuizValues.answers.map((el, j) => (j === i) ? ch : el)
+        setNewQuizValues({...newQuizValues, answers: a2})
+    }
+    const refresh = () => {
+        if (!newQuiz) setNewQuiz([ newQuizValues])
+        else setNewQuiz([...newQuiz, newQuizValues])
+        setNewQuizValues(a12)
+        console.log(newQuiz)
+    }
+    const save = () => {
+        if (newQuiz)
+        QuizDates.push(newQuiz)
+    }
+
     return (
-        <div>
-            ssssssss
+        <div className={styles.quiz} style={{paddingBottom: "50px"}}>
+            <div className={styles.container} style={{height: "500px"}}>
+                <div className={styles.newQuestion}>
+                    <input placeholder="insert question..."
+                           type="text"
+                           value={newQuizValues.questionText}
+                           onChange={e => setNewQuizValues({...newQuizValues, questionText: e.target.value})}
+                           className={styles.input} style={{width: "90%"}}
+                    />
+                </div>
+                <div className={styles.answerPR}>
+                    {inputs.map((inp, index) => (
+                        <div key={inp}>
+                            <input
+                                value={newQuizValues.answers[index].answer}
+                                onChange={(e) => changeArr(e, index)}
+                                placeholder="insert answer..."
+                                className={styles.input} style={{width: "70%"}}
+                                type="text"/>
+                            <input type="checkbox" checked={inp === isChecked} onChange={() => setIsChecked(inp)}/>
+                        </div>
+                    ))}
+                    <div>
+                        <button className={styles.addQuizBtn}
+                              onClick={save}  style={{backgroundColor: "rgba(136, 134, 134, 0.52)"}}>save
+                        </button>
+                        <button onClick={refresh} className={styles.addQuizBtn}>+</button>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
--- src/projects/quizProject/components/Register.tsx
+++ src/projects/quizProject/components/Register.tsx
@@ -18,19 +18,19 @@ const Register = () => {
                     <label htmlFor="name">Name</label>
                 </div>
                 <div>
-                    <input type="text" value={newUser.name} onChange={e =>setNewUser({...newUser,name: e.target.value})}/>
+                    <input className={styles.input} type="text" value={newUser.name} onChange={e =>setNewUser({...newUser,name: e.target.value})}/>
                 </div>
                 <div>
                     <label htmlFor="name">Email</label>
                 </div>
                 <div>
-                    <input type="text" value={newUser.email} onChange={e =>setNewUser({...newUser,email: e.target.value})}/>
+                    <input className={styles.input} type="text" value={newUser.email} onChange={e =>setNewUser({...newUser,email: e.target.value})}/>
                 </div>
                 <div>
                     <label htmlFor="name">Password</label>
                 </div>
                 <div>
-                    <input type="password" value={newUser.password} onChange={e =>setNewUser({...newUser,password: e.target.value})}/>
+                    <input className={styles.input} type="password" value={newUser.password} onChange={e =>setNewUser({...newUser,password: e.target.value})}/>
                 </div>
                 <div>
                     <button className={styles.btn} onClick={register} >register</button>
